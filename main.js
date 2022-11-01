b497164adf1367645678a6f94f458ff2db90c4d0
Macic <115644123+MacicLabs@users.noreply.github.com>
2022-11-01 20:07:43 +0100

    Expand L-systems

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import "./style.css"
+
+const expandSystem = (axiom, rules, iterations) => {
+	let newAxiom = axiom;
+	for (let i = 0; i < iterations; i++) {
+		let tmpAxiom = "";
+		for (let j of newAxiom) {
+			if (Object.keys(rules).includes(j)) {
+				tmpAxiom += rules[j];
+			}
+			else {
+				tmpAxiom += j;
+			}
+		}
+		newAxiom = tmpAxiom;
+	}
+	return newAxiom;
+}
