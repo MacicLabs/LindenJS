9a9b9f23c15acaac3a64e1da5abb2e7b0654bbbc
Macic <115644123+MacicLabs@users.noreply.github.com>
2022-11-01 20:50:29 +0100

    Draw with turtle graphics

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 import "./style.css"
 
+class Turtle {
+	constructor(ctx, x=0, y=0, angle=90*Math.PI/180, history=[]) {
+		this.ctx = ctx;
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+		this.history = history;
+	}
+
+	fd(size) {
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.x, this.y);
+		const dx = Math.cos(this.angle) * size;
+		const dy = Math.sin(this.angle) * size;
+		this.x += dx;
+		this.y += dy;
+		this.ctx.lineTo(this.x, this.y);
+		this.ctx.stroke();
+	}
+
+	bk(size) {
+		this.fd(-size);
+	}
+
+	lt(angle) {
+		this.angle -= angle * Math.PI / 180
+	}
+
+	rt(angle) {
+		this.lt(-angle);
+	}
+
+	save() {
+		this.history.push([this.x, this.y, this.angle]);
+	}
+
+	restore() {
+		let previousX, previousY, previousAngle;
+		[previousX, previousY, previousAngle] = this.history.pop();
+		this.x = previousX;
+		this.y = previousY;
+		this.angle = previousAngle;
+	}
+}
+
 const parseRules = (rules) => {
 	let rulesDict = {};
 	for (let i of rules.split("\n")) {
@@ -25,6 +70,39 @@ const expandSystem = (axiom, rules, iterations) => {
 		newAxiom = tmpAxiom;
 	}
 	return newAxiom;
+}
+
+const drawSystem = (string, angle, size, ctx) => {
+	ctx.clearRect(-(ctx.canvas.width / 2), (ctx.canvas.height / 2), ctx.canvas.width, -(ctx.canvas.height));
+	const turtle = new Turtle(ctx);
+	for (let i of string) {
+		switch (i) {
+			case "F": {
+				turtle.fd(size);
+				break;
+			}
+			case "f": {
+				turtle.bk(size);
+				break;
+			}
+			case "+": {
+				turtle.lt(angle);
+				break;
+			}
+			case "-": {
+				turtle.rt(angle);
+				break;
+			}
+			case "[": {
+				turtle.save();
+				break;
+			}
+			case "]": {
+				turtle.restore();
+				break;
+			}
+		}
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
