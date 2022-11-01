90606113521acd024b2c85daf3a9fc67e7f9eb2e
Macic <115644123+MacicLabs@users.noreply.github.com>
2022-11-01 20:28:15 +0100

    Add UI

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import "./style.css"
 
+const parseRules = (rules) => {
+	let rulesDict = {};
+	for (let i of rules.split("\n")) {
+		let key, value;
+		[key, value] = i.split("=");
+		rulesDict[key] = value;
+	}
+	return rulesDict;
+}
+
 const expandSystem = (axiom, rules, iterations) => {
 	let newAxiom = axiom;
 	for (let i = 0; i < iterations; i++) {
@@ -16,3 +26,22 @@ const expandSystem = (axiom, rules, iterations) => {
 	}
 	return newAxiom;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	const axiom = document.getElementById("axiom");
+	const rules = document.getElementById("rules");
+	const iterations = document.getElementById("iterations");
+	const angle = document.getElementById("angle");
+	const size = document.getElementById("size");
+	const draw = document.getElementById("draw");
+
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	ctx.translate(canvas.width / 2, canvas.height / 2);
+	ctx.scale(1, -1);
+	document.body.prepend(canvas);
+
+	draw.onclick = () => drawSystem(expandSystem(axiom.value, parseRules(rules.value), iterations.valueAsNumber), angle.valueAsNumber, size.valueAsNumber, ctx);
+})
